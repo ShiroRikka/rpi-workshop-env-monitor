7f66caf4fb901d40fafaff96c5b11cbaa199b771
ShiroRikka <shirorikka@qq.com>
2025-09-18 17:29:25 +0800

    ✨ feat(数据库): 创建数据库管理器
    - 新增 DatabaseManager 类，用于管理数据库连接和操作
    - 使用 pymysql 库连接 MySQL 数据库
    - 实现数据库和表的初始化，如果不存在则创建
    - 添加插入环境数据的方法 insert_env_data，用于将温度和湿度数据存入数据库
    - 使用 tenacity 库实现数据库连接的重试机制，提高稳定性
    - 删除旧的 EnvironmentDatabaseManager 类
    - 更新 pyproject.toml 文件，将项目版本更新为 1.0.0，并添加 tenacity 依赖
    - 更新 uv.lock 文件，同步依赖项

---
+++ DatabaseManager.py
@@ -0,0 +1,63 @@
+import pymysql
+from loguru import logger
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+
+class DatabaseManager:
+    def __init__(self, host: str, port: int, user: str, password: str = ""):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self._init_database()
+
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+    def _get_connection(self, database=None):
+        return pymysql.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            database=database,
+            connect_timeout=5,
+            read_timeout=5,
+            write_timeout=5,
+            autocommit=True,
+        )
+
+    def _init_database(self):
+        logger.info("正在初始化数据库环境...")
+        with self._get_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("CREATE DATABASE IF NOT EXISTS rpi_env_monitor")
+        with self._get_connection("rpi_env_monitor") as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS environment_data (
+                                id INT AUTO_INCREMENT PRIMARY KEY,
+                                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                                temperature FLOAT NOT NULL,
+                                humidity FLOAT NOT NULL
+                            )
+                        """)
+        logger.success("数据库初始化成功")
+
+    def insert_env_data(self, temp: float, humid: float | int):
+        """
+        将传入数据存入数据库中
+        :param temp: 温度
+        :param humid: 湿度
+        """
+        with self._get_connection("rpi_env_monitor") as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO environment_data (temperature, humidity) VALUES (%s, %s)
+                """,
+                    (temp, humid),
+                )
+
+
+if __name__ == "__main__":
+    db = DatabaseManager("127.0.0.1", 3306, "admin", "123456")
+    db.insert_env_data(23.5, 45)
