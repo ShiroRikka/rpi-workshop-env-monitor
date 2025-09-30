0fa13684ff0d03cba5b8d6153eeab398f2e83911
ShiroRikka <shirorikka@qq.com>
2025-10-01 00:10:35 +0800

    ✨ feat(devices): add relay control based on temperature
    - 实现基于温度的继电器控制
    - 当温度高于 30 度时，打开继电器
    - 当温度低于 30 度时，关闭继电器
    - 修改 devices 下的 init 文件，使用相对引用

--- devices/__init__.py
+++ devices/__init__.py
@@ -1,4 +1,4 @@
-from dht import rpi_dht11
-from relay import rpi_relay
+from .dht import rpi_dht11
+from .relay import rpi_relay
 
 __all__ = ["rpi_dht11", "rpi_relay"]
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import time
 import board
-from devices import rpi_dht11, rpi_relay
+from devices import relay, rpi_dht11, rpi_relay
 
 # 主程序
 if __name__ == "__main__":
     dht11 = rpi_dht11(board.D14)
+    relay = rpi_relay(15)
     while True:
-        dht11.read()
-        time.sleep(1)
+        tem,hum = dht11.read()
+        if tem > 30:
+            relay.on()
+        else:
+            relay.off()
+
+        time.sleep(2)
+
