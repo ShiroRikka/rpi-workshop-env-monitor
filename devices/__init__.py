7ea76889aeb10c697264e76a7e5c4de1ca58eb29
ShiroRikka <shirorikka@qq.com>
2025-10-01 12:20:12 +0800

    ✨ feat(devices): 将 DatabaseManager 添加到 devices 模块
    - 将 DatabaseManager 添加到 `devices` 模块的 `__all__` 列表中
    - 允许在其他模块中直接导入和使用 DatabaseManager 类

--- devices/__init__.py
+++ devices/__init__.py
@@ -1,4 +1,5 @@
 from .dht import rpi_dht11
 from .relay import rpi_relay
+from .databasemanager import DatabaseManager
 
-__all__ = ["rpi_dht11", "rpi_relay"]
+__all__ = ["rpi_dht11", "rpi_relay", "DatabaseManager"]
